348cf6ef25e5b03831fa447261bd2849a84f255e
layersony <layersony@gmail.com>
2022-06-16 06:12:13 +0300

    finalized on project

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,37 @@
-import React from "react";
-import { v4 as uuid } from "uuid";
+import React, {useState} from "react";
+import { v4 as uuid } from "uuid"; // creates a uunique key
 
-function ItemForm(props) {
+function ItemForm({onItemSubmission}) {
+  const [name, setName] = useState("")
+  const [category, setCategory] = useState("Produce")
+
+  function handleNameChange(event){
+    setName(event.target.value)
+  }
+
+  function handleCategoryChange(event) {
+    setCategory(event.target.value)
+  }
+
+  function handleSubmitData(event){
+    event.preventDefault();
+    onItemSubmission({
+      id: uuid(),
+      name,
+      category
+    }) // back in app,js newitem is an object so onItemSubmission has to be an object
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmitData}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" value={name} onChange={handleNameChange} />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" value={category} onChange={handleCategoryChange}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
